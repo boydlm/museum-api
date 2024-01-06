4ce822879021de63311d5170575c6329019cec5c
Leah Boyd <boydlm@outlook.com>
2024-01-06 16:32:39 -0500

    added di

--- api/services/museum_factory.py
+++ api/services/museum_factory.py
@@ -2,16 +2,19 @@ from api.services.museum_api import Museum
 import os
 from api.constants.museum_types import MuseumTypes
 
-def create_museum(museum_type: str):
-    if museum_type == MuseumTypes.Harvard.value:
-        url=harvard_url()
-        return Museum(url)
+
+class MuseumFactory:
+    def __init__(self, harvard_apikey: str, harvard_url: str) -> None:
+        self._harvard_apikey = harvard_apikey
+        self._harvard_url = harvard_url
+
+    def create_museum(self, museum_type: str):
+        if museum_type == MuseumTypes.Harvard.value:
+            url=self.harvard_url()
+            return Museum(url)
     
-def harvard_url():
-    base_url = "https://api.harvardartmuseums.org"
-    endpoint = "/exhibition"
-    api_key = os.getenv("HARVARD_API_KEY")
-    url = f"{base_url}{endpoint}?apikey={api_key}&status=current"
-    print(url)
-    return url
+    def harvard_url(self):
+        url = f"{self._harvard_url}?apikey={self._harvard_apikey}&status=current"
+        print(url)
+        return url
 
