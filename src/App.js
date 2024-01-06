6b468b04124630a682b42645fb1b5b250dae32d0
Leah Boyd <114845124+boydlm@users.noreply.github.com>
2024-01-06 17:00:57 -0500

    displays exhibitions on frontend (#2)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Link } from '@mui/material';
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
+  const [exhibitions, setExhibitions] = useState([]);
 
   useEffect(() => {
     fetch('/exhibitions/harvard').then(res => res.json()).then(data => {
-      setCurrentTime("hello");
+      setExhibitions(data.records);
       console.log(data)
     });
   }, []);
@@ -15,19 +15,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is {currentTime}.</p>
+
+        {exhibitions.map((exhibition) => (
+          <Link href={exhibition.url}> {exhibition.title} </Link>  
+  ))}
+       
       </header>
     </div>
   );
